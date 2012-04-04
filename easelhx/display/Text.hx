0d84924e7cd9f8e52e8f0f1b40ce44e73d48e833
Phil Douglas <ricick@gmail.com>
2012-04-04 21:17:06 +1000

    added text

--- easelhx/display/Text.hx
+++ easelhx/display/Text.hx
@@ -1,6 +1,7 @@
-/**
-* Bitmap by Grant Skinner. Dec 5, 2010
-* Visit www.gskinner.com/blog for documentation, updates and more free code.
+package easelhx.display;
+/*
+* Text by Grant Skinner. Dec 5, 2010
+* Visit http://easeljs.com/ for documentation, updates and examples.
 *
 *
 * Copyright (c) 2010 Grant Skinner
@@ -25,58 +26,116 @@
 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 * OTHER DEALINGS IN THE SOFTWARE.
-**/
-package easelhx.display;
+*/
 
+/**
+* The Easel Javascript library provides a retained graphics mode for canvas 
+* including a full, hierarchical display list, a core interaction model, and 
+* helper classes to make working with Canvas much easier.
+* @module EaselJS
+**/	
+/**
+* Allows you to display one or more lines of dynamic text (not user editable) in the display list.
+* Line wrapping support (using the lineWidth is very basic, wrapping on spaces and tabs only. Note
+* that as an alternative to Text, you can position HTML text above or below the canvas relative to 
+* items in the display list using the localToGlobal() method.
+* @class Text
+* @extends DisplayObject
+* @constructor
+* @param {String} text Optional. The text to display.
+* @param {String} font Optional. The font style to use. Any valid value for the CSS font attribute is 
+* acceptable (ex. "36px bold Arial").
+* @param {String} color Optional. The color to draw the text in. Any valid value for the CSS color attribute
+* is acceptable (ex. "#F00").
+**/
 @:native("Text")
 extern class Text extends DisplayObject {
-
+	
+	public function new(text:String, font:String, color:String):Void;
 // public properties:
-	/** The text to display. **/
-	public var text( default, default ) : String;
-	
-	/** The font style to use. Any valid value for the CSS font attribute is acceptable (ex. "bold 36px Arial"). **/
-	public var font( default, default ) : String;
-	
-	/** The color to draw the text in. Any valid value for the CSS color attribute is acceptable (ex. "#F00"). **/
-	public var color( default, default ) : String;
-	
-	/** The horizontal text alignment. Any of start, end, left, right, and center. For detailed information 
-	* view the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0">whatwg spec</a>. **/
-	public var textAlign( default, default ) : String;
-	
-	/** The vertical alignment point on the font. Any of top, hanging, middle, alphabetic, ideographic, or bottom. For detailed information 
-	* view the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0">whatwg spec</a>. **/
-	public var textBaseline( default, default ) : String;
-	
-	/** The maximum width to draw the text. If maxWidth is specifiied (not null), the text will be condensed 
-	* or shrunk to make it fit in this width. For detailed information view the 
-	* <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0">whatwg spec</a>. **/
-	public var maxWidth( default, default ) : Float;
-	
-	/** If true, the text will be drawn as a stroke (outline). If false, the text will be drawn as a fill. **/
-	public var outline( default, default ) : Bool;
-	
-// constructor:
 	/**
-	* Constructs a new Text instance.
-	* @param text Optional. The text to display.
-	* @param font Optional. The font style to use. Any valid value for the CSS font attribute is acceptable (ex. "36px bold Arial").
-	* @param color Optional. The color to draw the text in. Any valid value for the CSS color attribute is acceptable (ex. "#F00").
-	* @class Allows you to display a single line of dynamic text (not user editable) in the display list. Note that as an alternative to 
-	* Text, you can position HTML text above or below the canvas relative to items in the display list using the localToGlobal() method.
-	* @augments DisplayObject
-	**/
-	public function new( text : String, font : String, ?color : String ) : Void;
-	
+	 * The text to display.
+	 * @property text
+	 * @type String
+	 **/
+	public var text:String;
 	
 	/**
-	* Returns the measured, untransformed width of the text.
-	**/
-	public function getMeasuredWidth() : Float;
+	 * The font style to use. Any valid value for the CSS font attribute is acceptable (ex. "bold 36px Arial"). 
+	 * @property font
+	 * @type String
+	 **/
+	public var font:String;
 	
-	override public function clone() : DisplayObject;
-		
-	override public function toString() : String;
+	/**
+	 * The color to draw the text in. Any valid value for the CSS color attribute is acceptable (ex. "#F00").
+	 * @property color
+	 * @type String
+	 **/
+	public var color:String;
 	
+	/**
+	 * The horizontal text alignment. Any of "start", "end", "left", "right", and "center". For detailed 
+	 * information view the 
+	 * <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0">
+	 * whatwg spec</a>.
+	 * @property textAlign
+	 * @type String
+	 **/
+	public var textAlign:String;
+	
+	/** The vertical alignment point on the font. Any of "top", "hanging", "middle", "alphabetic", 
+	 * "ideographic", or "bottom". For detailed information view the 
+	 * <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0">
+	 * whatwg spec</a>.
+	 * @property textBaseline
+	 * @type String
+	*/
+	public var textBaseline:String;
+	
+	/** The maximum width to draw the text. If maxWidth is specified (not null), the text will be condensed or 
+	 * shrunk to make it fit in this width. For detailed information view the 
+	 * <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0">
+	 * whatwg spec</a>.
+	 * @property maxWidth
+	 * @type Number
+	*/
+	public var maxWidth:Float;
+	
+	/** If true, the text will be drawn as a stroke (outline). If false, the text will be drawn as a fill.
+	 * @property outline
+	 * @type Boolean
+	 **/
+	public var outline:Bool;
+	
+	/** Indicates the line height (vertical distance between baselines) for multi-line text. If null, 
+	 * the value of getMeasuredLineHeight is used.
+	 * @property lineHeight
+	 * @type Number
+	 **/
+	public var lineHeight:Float;
+	
+	/**
+	 * Indicates the maximum width for a line of text before it is wrapped to multiple lines. If null, 
+	 * the text will not be wrapped.
+	 * @property lineWidth
+	 * @type Number
+	 **/
+	public var lineWidth:Float;
+	
+	/**
+	 * Returns the measured, untransformed width of the text.
+	 * @method getMeasuredWidth
+	 * @return {Number} The measured, untransformed width of the text.
+	 **/
+	public function getMeasuredWidth():Float;
+
+	/**
+	 * Returns an approximate line height of the text, ignoring the lineHeight property. This is based 
+	 * on the measured width of a "M" character multiplied by 1.2, which approximates em for most fonts.
+	 * @method getMeasuredLineHeight
+	 * @return {Number} an approximate line height of the text, ignoring the lineHeight property. This is 
+	 * based on the measured width of a "M" character multiplied by 1.2, which approximates em for most fonts.
+	 **/
+	public function getMeasuredLineHeight():Float;
 }
